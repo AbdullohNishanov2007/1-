1c3f6f8526e398f47567840bd8f777b1e2fcba2c
abdullo Nishanov <abdullohnishanov2007@gmail.com>
2025-11-14 17:10:17 +0600

    поменял шрифот на Montserrat

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Services from './pages/Services/Services';
+import Contact from './pages/Contact/Contact';
 import './App.css';
 
 function App() {
